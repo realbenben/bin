8e46c69a950f1e5bebef35a8aeae4674aae970c6
David Leuliette <dleuliette@gmail.com>
2020-07-30 14:04:44 +0200

    Organize stuff like a boss

---
+++ index.js
@@ -0,0 +1,6 @@
+var math = 1;
+var result = math++;
+alert(result);
+var prenom = prompt('Quel est votre prénom ?');
+alert(prenom);
+console.log(prenom);
--- test avechtml.js
+++
@@ -1,7 +0,0 @@
-var math = 1;
-var result = math++  ;
-alert(result);
-var prenom = prompt("Quel est votre prénom ?");
-alert(prenom);
-console.log(prenom
-);
