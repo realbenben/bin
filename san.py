aff30ec2ff671b1ad2f39c5345ba4d3ec2388957
realbenben <b.patou59@gmail.com>
2020-09-28 11:06:44 +0200

    add test file

---
+++ san.py
@@ -0,0 +1,40 @@
+[{"character": "alvin et les Chimpunks"},
+ {"character":"ma bite"}
+ {"character":"vladimir Poutine"}
+ {"character":"Donald Trump"}
+ {"character":"Rick Hunter"}
+ {"character":"Kratos"}
+ {"character":"Cartman"}
+ {"character":"Link"}
+ {"character":"Ghandi"}
+ {"character":"Mickael Jackson"}
+ {"character":"Sheldon"}
+ {"character":"Iron Man"}
+ {"character":"Ta mere"}
+ {"character":"Patrick Sebastien"}
+ {"character":"Sami nasceri"}
+ {"character":"kirikou"}
+ {"character":"Titus Andromédon"}
+ {"character":"quelqun"}
+{"character": "Babar"}]
+
+###############################################################
+[{"quote": "Ecoutez-moi, Monsieur Shakespeare, nous avons beau être ou ne pas être, nous sommes !"},
+{"quote": "On doit pouvoir choisir entre s'écouter parler et se faire entendre."}
+{"quote":"Témoignez du respect au fat, et bientôt il se croira tout permis."}
+{"quote": "Un pédant, enivré de sa vaine science, n'a souvent fait qu'un sot."}
+{"quote":"Mieux vaut être fou avec tous que sage tout seul."}
+{"quote":"On ne sympathise pas avec le niais, ni avec le sourire à tout venant."}
+{"quote":"Il y a beaucoup d'idiots en ce monde, il faut s'habituer."}
+{"quote":"Un sot plein de savoir est plus sot qu'un autre homme."}
+{"quote":"Mieux vaut être stérilisé par l'intelligence que fécondé par la sottise."}
+{"quote":"Le con ne perd jamais son temps, il perd celui des autres."}
+{"quote":"La connerie humaine est indélébile !"}
+{"quote":"L'abruti ne voit le beau que dans les belles choses"}
+{"quote":" Le stupide est celui qui s'étonne de tout, et s'émerveille de tout."}
+{"quote":"Le ridicule, au lieu de tuer certains, les engraisse."}
+{"quote":"Être innocent c'est très joli, mais il ne faut pas en abuser."}
+{"quote":"Le despote coupe l'arbre pour avoir le fruit."}
+{"quote":"La pire des tyrannies est celle qui opprime la pensée."}
+{"quote":"Un sot est arrogant, un savant est timide. "}
+{"quote":"Le vulgaire n'est composé que de sots. "}]
--- san_antonio.py
+++ san_antonio.py
@@ -1,15 +1,36 @@
 # -*- coding: utf8 -*-
-quotes = [
-    "Ecoutez-moi, Monsieur Shakespeare, nous avons beau être ou ne pas être, nous sommes !",
-    "On doit pouvoir choisir entre s'écouter parler et se faire entendre."
-]
+import random
+import json
 
-characters = [
-    "alvin et les Chipmunks",
-    "Babar",
-    "betty boop",
-    "calimero",
-    "casper",
-    "le chat potté",
-    "Kirikou"
-]
+def read_values_from_json(file, key):
+    values = []
+    with open(file) as f:
+        data = json.load(f)
+        for entry in data:
+            values.append(entry[key])
+    return values
+
+def message(character, quote):
+    n_character = character.capitalize()
+    n_quote = quote.capitalize()
+    return "{} a dit : {}".format(n_character, n_quote)
+
+def get_random_item_in(my_list):
+    rand_numb = random.randint(0, len(my_list) - 1)
+    item = my_list[rand_numb] # get a quote from a list
+    return item # return the item
+
+def get_random_quote():
+    all_values = read_values_from_json('quotes.json', 'quote')
+    return get_random_item_in(all_values)
+
+def get_random_character():
+    all_values = read_values_from_json('characters.json', 'character')
+    return get_random_item_in(all_values)
+
+# Programm
+user_answer = input('Tapez entrée pour connaître une autre citation ou B pour quitter le programme.')
+
+while user_answer != "B":
+    print(message(get_random_character(), get_random_quote()))
+    user_answer = input('Tapez entrée pour connaître une autre citation ou B pour quitter le programme.')
